492902d0fb473adbc0f7d1fd73492615d31a6274
iramos <igor.ramos@capgemini.com>
2023-07-08 14:29:49 -0300

    FixIssueGItPages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxSpinnerModule } from 'ngx-spinner';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { PokedexModule } from './pokedex/pokedex.module';
-import { HttpRequestInterceptor } from './util/index';
 
 @NgModule({
   declarations: [
@@ -26,7 +26,7 @@ import { HttpRequestInterceptor } from './util/index';
     PokedexModule
   ],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: HttpRequestInterceptor, multi: true }
+    {provide: LocationStrategy, useClass: HashLocationStrategy}
   ],
   bootstrap: [AppComponent]
 })
